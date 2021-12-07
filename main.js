87194680b61b2984b04535fe2222e5f48c9aa06d
Bin Shu <shubinlin@gmail.com>
2021-12-06 17:13:32 -0700

    adding more instructions of how to install and setup

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const {google} = require('googleapis');
 const keys = require("./keys.json");
 
+// install: npm install googleapis@39 --save
 //console.developers.google.com
 //https://developers.google.com/identity/protocols/oauth2/scopes
+//Create credentials: https://developers.google.com/workspace/guides/create-credentials?authuser=1#web
+
 
 const client = new google.auth.JWT(
     keys.client_email,
