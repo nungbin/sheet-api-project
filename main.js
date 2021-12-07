985f689c0499e093c729e64bd48b345a74e5186a
Bin Shu <shubinlin@gmail.com>
2021-12-06 18:04:04 -0700

    thanks to the dude. the Google sheet API is working

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const keys = require("./keys.json");
 //console.developers.google.com
 //https://developers.google.com/identity/protocols/oauth2/scopes
 //Create credentials: https://developers.google.com/workspace/guides/create-credentials?authuser=1#web
-
+// Service Email ID: serviceman11@youtube-kodi-151713.iam.gserviceaccount.com
 
 const client = new google.auth.JWT(
     keys.client_email,
@@ -21,5 +21,46 @@ client.authorize(function(err, tokens) {
     }
     else {
         console.log('Connected!');
+        gsrun(client);
     }
 });
+
+
+async function gsrun(cl) {
+    const gsapi = google.sheets({
+        version: 'v4',
+        auth: cl
+    });
+
+    const opt = {
+        spreadsheetId: '116rgysh5iI8s__LBiDue7PJSiuB1Dn2NVkurN27qF78',
+        range: 'Data!A2:B6'
+    };
+
+    let data = await gsapi.spreadsheets.values.get(opt);
+    let dataArray = data.data.values;
+
+    // taking care of cells with blank values
+    dataArray.map(function(r) {
+        while(r.length < 2) {
+            r.push('');
+        };
+        return r;
+    });
+
+    let newDataArray = dataArray.map(function(r) {
+        r.push(r[0] + '-' + r[1]);
+        return r;
+    });
+    console.log(newDataArray);
+
+    const updateOptions = {
+        spreadsheetId: '116rgysh5iI8s__LBiDue7PJSiuB1Dn2NVkurN27qF78',
+        range: 'Data!E2',
+        valueInputOption: 'USER_ENTERED',
+        resource: { values: newDataArray}
+    };
+
+    let res = await gsapi.spreadsheets.values.update(updateOptions);
+    console.log(res);
+}
